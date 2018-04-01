ccb6264d9a6f5a90a0ec9d87a0f5aeb842ff4b1a
Steven Chen <ysteven.chen2@gmail.com>
2018-04-01 06:32:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ if(!cardNumber){
   return "Sorry, we don't have a credit card on file for you."
 }
 else{
-  const ("Your total cost is $"+total()+", which will be charged to the card "+cardNumber+".");
+  console.log("Your total cost is $"+total()+", which will be charged to the card "+cardNumber+".");
   cart.splice(0,cart.length);
   return cart;
 }
