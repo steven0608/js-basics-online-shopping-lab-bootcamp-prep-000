9a75311c11835416e363748ed05674b87cbfaff8
Steven Chen <ysteven.chen2@gmail.com>
2018-04-01 06:13:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,5 +51,11 @@ function removeFromCart(item){
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+if(!cardNumber){
+  return "Sorry, we don't have a credit card on file for you."
+}
+else{
+  Cart=[];
+  return ()"Your total cost is $"+total+", which will be charged to the card "+cardNumber+".");
+}
 }
