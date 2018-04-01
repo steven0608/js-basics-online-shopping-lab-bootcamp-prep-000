9cab8b2a210fea535b328de9c65e6652bf171cde
Steven Chen <ysteven.chen2@gmail.com>
2018-04-01 06:35:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,12 +51,12 @@ function removeFromCart(item){
 }
 
 function placeOrder(cardNumber) {
-if(!cardNumber){
-  return "Sorry, we don't have a credit card on file for you."
+if(cardNumber){
+  return("Your total cost is $"+total()+", which will be charged to the card "+cardNumber+".");
+cart.splice(0,cart.length);
+cart=[];
 }
 else{
-  return("Your total cost is $"+total()+", which will be charged to the card "+cardNumber+".");
-  cart.splice(0,cart.length);
-  cart=[];
+  return "Sorry, we don't have a credit card on file for you."
 }
 }
