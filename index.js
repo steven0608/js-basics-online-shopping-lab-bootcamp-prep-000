645b7e5eeb8e28fcb29e35327748379a53f1c5d7
Steven Chen <ysteven.chen2@gmail.com>
2018-04-01 06:13:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,6 +56,6 @@ if(!cardNumber){
 }
 else{
   Cart=[];
-  return ()"Your total cost is $"+total+", which will be charged to the card "+cardNumber+".");
+  return ("Your total cost is $"+total+", which will be charged to the card "+cardNumber+".");
 }
 }
