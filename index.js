e2698ce721de61fbe3101ae9cfbe08178baa23e3
Steven Chen <ysteven.chen2@gmail.com>
2018-04-01 06:14:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ if(!cardNumber){
   return "Sorry, we don't have a credit card on file for you."
 }
 else{
-  return ("Your total cost is $"+total+", which will be charged to the card "+cardNumber+".");
+  return ("Your total cost is $"+total()+", which will be charged to the card "+cardNumber+".");
 }
 }
