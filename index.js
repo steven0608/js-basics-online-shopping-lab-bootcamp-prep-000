3c982b3e0cf9e4a5ddb5a1989d870d744a07ced0
Steven Chen <ysteven.chen2@gmail.com>
2018-04-01 05:43:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,8 +45,7 @@ function removeFromCart(item) {
     if (cart[i].hasOwnProperty(item)===true){
     return cart.splice(i,1)
   }
-  return "That item is not in your cart."
-  }
+}
 }
 
 function placeOrder(cardNumber) {
