02be90fd998283731f55043053039c09a8eb4618
Steven Chen <ysteven.chen2@gmail.com>
2018-04-01 05:31:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ return sum;
 function removeFromCart(item) {
   for (let i=0;i<cart.length;i++) {
     if (cart[i]["itemName"]===item){
-    cart.splice(i,0)
+    return cart.splice(i,1)
   }else{
     return "That item is not in your cart."
   }
